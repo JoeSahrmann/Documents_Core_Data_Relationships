74ca76386b1fa4fb8c2643fb0983fb90afc8f0e6
joe sahrmann <joexs10@gmail.com>
2020-09-28 16:33:19 -0500

    updated the core data files

--- Category+CoreDataClass.swift
+++ Category+CoreDataClass.swift
@@ -6,10 +6,21 @@
 //
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 @objc(Category)
 public class Category: NSManagedObject {
 
+    var documents: [Document]?{
+        return self.rawDocument?.array as? [Document]
+    }
+    convenience init?(title: String){
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        guard let context = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        self.init(entity: Category.entity(), insertInto: context)
+        self.title = title
+    }
 }
--- Document+CoreDataClass.swift
+++ Document+CoreDataClass.swift
@@ -6,10 +6,41 @@
 //
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 @objc(Document)
 public class Document: NSManagedObject {
 
+    //this gets and set the date
+    var date: Date?{
+        get{
+            return rawDate as Date?
+        }
+        set{
+            rawDate = newValue as Date?
+        }
+    }
+    var size: Int64?{
+        get{
+            return rawSize as Int64?
+        }
+        set{
+            rawSize = newValue as Int64? ?? 0
+        }
+    }
+    //this intializes the class properties
+    convenience init?(name: String?, content: String?, size: Int64, date:Date?){
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        guard let context = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        self.init(entity: Document.entity(), insertInto: context)
+        self.name = name
+        self.content = content
+        self.size = size
+        self.date = date
+        
+    }
 }
